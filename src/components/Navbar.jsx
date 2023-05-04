6b1655a7e4db61e88ca2b5fa488991b6e847d710
Diego Garcia <diegogarciaq98@gmail.com>
2023-05-04 14:21:52 -0500

    Adding project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+import { Bars3Icon } from "@heroicons/react/24/outline";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="flex justify-between items-center h-16 bg-white text-black relative
+      shadow-sm font-mono"
+      role="navigation"  
+    >
+      <Link to="/" className="pl-8">
+        Egg
+      </Link>
+      <div className="px-4 cursor-pointer md:hidden">
+        <Bars3Icon className="h-6 w-6 text-gray-500" />
+      </div>
+      <div className="pr-8 md:block hidden">
+        <Link className="p-4" to="/">
+          Home
+        </Link>
+        <Link className="p-4" to="/menu">
+          Menu
+        </Link>
+        <Link className="p-4" to="/about">
+          About
+        </Link>
+        <Link className="p-4" to="/contact">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
